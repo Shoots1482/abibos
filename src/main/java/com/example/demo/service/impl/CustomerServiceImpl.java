8702a8f1e9d606cca1da253a02b928ac644072eb
OmarElhagagy <oadeell21@gmail.com>
2025-05-21 05:36:48 +0300

    Fixes

--- src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.demo.repositories.CustomerOrderRepository;
 import com.example.demo.service.CustomerService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
